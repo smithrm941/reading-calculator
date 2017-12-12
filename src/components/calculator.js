9828395f26e37c8427c098b7f770fa8f8e543167
smithrm941 <smithrm941@gmail.com>
2017-12-12 10:01:44 -0800

    react-create-app on Heroku

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -9,7 +9,7 @@ class Calculator extends Component {
     const timeFrameNumber = this.props.timeFrameNumber
     const timeFrameType = this.props.timeFrameType
     const singularTimeFrameType = this.props.singularTimeFrameType
-    const pagesPerTimeFrame = Math.round(((pagesLeft/timeFrameNumber)*100)/100)
+    const pagesPerTimeFrame = Math.round(pagesLeft/timeFrameNumber)
 
     if(!totalPages || !currentPage) {
       return (
@@ -24,12 +24,28 @@ class Calculator extends Component {
         </div>
       )
     } else {
-      return (
-        <div>
-          <h1>Pages Left: {pagesLeft}</h1>
-          <h1>You can finish this book in {timeFrameNumber} {timeFrameType} by reading {pagesPerTimeFrame} pages per {singularTimeFrameType}</h1>
-        </div>
-      )
+      if(timeFrameType === "weeks") {
+        return (
+          <div>
+            <h1>Pages Left: {pagesLeft}</h1>
+            <h1>You can finish this book in {timeFrameNumber} {timeFrameType} by reading {Math.round(pagesPerTimeFrame/7)} pages per day.</h1>
+          </div>
+        )
+      } else if(timeFrameType === "months") {
+        return (
+          <div>
+            <h1>Pages Left: {pagesLeft}</h1>
+            <h1>You can finish this book in {timeFrameNumber} {timeFrameType} by reading {Math.round(pagesPerTimeFrame/30)} pages per day.</h1>
+          </div>
+        )
+      } else {
+        return (
+          <div>
+            <h1>Pages Left: {pagesLeft}</h1>
+            <h1>You can finish this book in {timeFrameNumber} {timeFrameType} by reading {pagesPerTimeFrame} pages per day.</h1>
+          </div>
+        )
+      }
     }
 
   }
