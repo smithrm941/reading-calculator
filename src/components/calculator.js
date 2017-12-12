1e82995bfe2689a153188d9d0f0ea3dd0d659581
smithrm941 <smithrm941@gmail.com>
2017-12-11 22:46:08 -0800

    got rid of decimals from page numbers, no longer says read infinity pages before typing in desired timeframe

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -9,11 +9,18 @@ class Calculator extends Component {
     const timeFrameNumber = this.props.timeFrameNumber
     const timeFrameType = this.props.timeFrameType
     const singularTimeFrameType = this.props.singularTimeFrameType
-    const pagesPerTimeFrame = pagesLeft/timeFrameNumber
+    const pagesPerTimeFrame = Math.round(((pagesLeft/timeFrameNumber)*100)/100)
 
     if(!totalPages || !currentPage) {
       return (
         <div>
+        </div>
+      )
+    } else if (!timeFrameNumber) {
+      return (
+        <div>
+          <h1>Pages Left: {pagesLeft}</h1>
+          <h1>You can finish this book in one {singularTimeFrameType} by reading {pagesLeft} pages per {singularTimeFrameType}</h1>
         </div>
       )
     } else {
