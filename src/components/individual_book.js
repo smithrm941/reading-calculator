cd59df24573a6a0d7e0b318b1859df7753cbf6e7
smithrm941 <smithrm941@gmail.com>
2017-12-22 11:09:35 -0800

    seem to have fixed issues with crashing if certain boxes were empty

--- src/components/individual_book.js
+++ src/components/individual_book.js
@@ -7,57 +7,106 @@ class IndividualBook extends Component {
   }
 
   render() {
-    //both totalPages and currentPage must be numbers
-    //if one of them is not, things crash!!!
+
     const selectedBook = this.props.selectedBook
+    const enteredBook = this.props.enteredBook
     const coverPhoto = this.props.coverPhoto
     const totalPages = this.props.totalPages
     const currentPage = this.props.currentPage
     const pagesLeft = this.props.pagesLeft
 
     if(selectedBook) {
-      return (
-      <div>
-        <form className="specific-book-data">
-          <h1>Selected Book: <input type="text" ref="title" value={selectedBook} onChange={this.handleChange.bind(this)} readOnly/></h1>
-          <img alt="No book selected or cover not available" src={coverPhoto} />
-          <h1>Total Pages: <input type="number" ref="total_pages" value={totalPages} onChange={this.handleChange.bind(this)} readOnly/></h1>
-          <h1>Pages Left: <input type="number" ref="pages" value={pagesLeft} onChange={this.handleChange.bind(this)} readOnly/></h1>
-        </form>
-      </div>
-      )
-    } else if (!selectedBook && pagesLeft) {
       if(pagesLeft >= 0) {
         return (
           <div>
-            {/* // <h1>Selected Book: {selectedBook.title}</h1>
-            // <img alt="No book selected or cover not available" src={selectedBook.thumbnail} /> */}
+            <form className="specific-book-data">
+              <h1>Selected Book: <input type="text" ref="title" value={selectedBook} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <img alt="No book selected or cover not available" src={coverPhoto} />
+              <h1>Total Pages: <input type="number" ref="total_pages" value={totalPages} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <h1>Pages Left: <input type="number" ref="pages" value={pagesLeft} onChange={this.handleChange.bind(this)} readOnly/></h1>
+            </form>
+          </div>
+        )
+      } else if (!totalPages && currentPage) {
+        return (
+          <div>
+            <form className="specific-book-data">
+              <h1>Selected Book: <input type="text" ref="title" value={selectedBook} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <img alt="No book selected or cover not available" src={coverPhoto} />
+              <h1>Total Pages: <input type="number" ref="total_pages" value={totalPages} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <h1>Please enter the total number of pages in the book you're reading.</h1>
+            </form>
+          </div>
+        )
+      } else if (totalPages && currentPage && currentPage > totalPages) {
+        return (
+          <div>
+            <form className="specific-book-data">
+              <h1>Selected Book: <input type="text" ref="title" value={selectedBook} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <img alt="No book selected or cover not available" src={coverPhoto} />
+              <h1>Total Pages: <input type="number" ref="total_pages" value={totalPages} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <h1>Current page cannot be higher than total pages.</h1>
+            </form>
+          </div>
+        )
+       }
+    } else if (enteredBook) {
+      if(pagesLeft >= 0) {
+        return (
+          <div>
+            <form className="specific-book-data">
+              <h1>Selected Book: <input type="text" ref="title" value={enteredBook} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <h1>Total Pages: <input type="number" ref="total_pages" value={totalPages} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <h1>Pages Left: <input type="number" ref="pages" value={pagesLeft} onChange={this.handleChange.bind(this)} readOnly/></h1>
+            </form>
+          </div>
+        )
+      } else if (!totalPages && currentPage) {
+        return (
+          <div>
+            <form className="specific-book-data">
+              <h1>Selected Book: <input type="text" ref="title" value={enteredBook} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <img alt="No book selected or cover not available" src={coverPhoto} />
+              <h1>Total Pages: <input type="number" ref="total_pages" value={totalPages} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <h1>Pages Left: <h1>Please enter the total number of pages in the book you're reading.</h1></h1>
+            </form>
+          </div>
+        )
+      } else if (totalPages && currentPage && currentPage > totalPages) {
+        return (
+          <div>
+            <form className="specific-book-data">
+              <h1>Selected Book: <input type="text" ref="title" value={enteredBook} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <img alt="No book selected or cover not available" src={coverPhoto} />
+              <h1>Total Pages: <input type="number" ref="total_pages" value={totalPages} onChange={this.handleChange.bind(this)} readOnly/></h1>
+              <h1>Pages Left: <h1>Current page cannot be higher than total pages.</h1></h1>
+            </form>
+          </div>
+        )
+      }
+    } else if ((!selectedBook || !enteredBook) && pagesLeft) {
+      if(pagesLeft >= 0) {
+        return (
+          <div>
             <h1>Pages Left: {pagesLeft}</h1>
           </div>
         )
       } else if (!totalPages && currentPage) {
         return (
           <div>
-            {/* // <h1>Selected Book: {selectedBook.title}</h1>
-            // <img alt="No book selected or cover not available" src={selectedBook.thumbnail} /> */}
             <h1>Please enter the total number of pages in the book you're reading.</h1>
           </div>
         )
       } else if (totalPages && currentPage && currentPage > totalPages) {
         return (
           <div>
-            {/* // <h1>Selected Book: {selectedBook.title}</h1>
-            // <img alt="No book selected or cover not available" src={selectedBook.thumbnail} /> */}
             <h1>Current page cannot be higher than total pages.</h1>
           </div>
         )
       }
-    } else if (!selectedBook && !totalPages) {
+    } else if (!totalPages) {
       return (
-      <div><h1>Enter some stuff...</h1></div>
-        // <h1>Selected Book: {selectedBook.title}</h1>
-        // <img alt="No book selected or cover not available" src={selectedBook.thumbnail} />
-        // <h1>Pages Left: {pagesLeft}</h1>
+      <div></div>
       )
     }
   }
