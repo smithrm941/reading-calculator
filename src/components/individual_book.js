54741cc14444a9799b7ae3f2ca0d3fd6d7998c4a
smithrm941 <smithrm941@gmail.com>
2017-12-21 16:24:20 -0800

    can save book data to database, WIP making that ability useful

--- src/components/individual_book.js
+++ src/components/individual_book.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react'
 
 class IndividualBook extends Component {
+
+  componentDidMount() {
+    console.log('Component has mounted')
+
+  }
+
+  addBook(event) {
+    event.preventDefault()
+    let data = {
+      title: this.refs.title.value,
+      pages: this.refs.pages.value
+    }
+    let request = new Request('http://localhost:3000/api/newBook', {
+      method: 'POST',
+      headers: new Headers({ 'Content-Type': 'application/json'}),
+      body: JSON.stringify(data)
+    })
+
+    fetch(request)
+      .then((response) => {
+        response.json()
+          .then((data) => {
+            console.log(data)
+          })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   render() {
     const selectedBook = this.props.selectedBook
     const coverPhoto = this.props.coverPhoto
@@ -11,9 +41,12 @@ class IndividualBook extends Component {
     if(selectedBook && pagesLeft) {
       return (
       <div>
-        <h1>Selected Book: {selectedBook}</h1>
-        <img alt="No book selected or cover not available" src={coverPhoto} />
-        <h1>Pages Left: {pagesLeft}</h1>
+        <form>
+          <h1>Selected Book: <input type="text" ref="title" value={selectedBook}/></h1>
+          <img alt="No book selected or cover not available" src={coverPhoto} />
+          <h1>Pages Left: <input type="number" ref="pages" value={pagesLeft}/></h1>
+          <button onClick={this.addBook.bind(this)}>Add Book</button>
+        </form>
       </div>
       )
     } else if (!selectedBook && totalPages && pagesLeft) {
